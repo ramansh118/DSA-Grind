6afb2742b790429974af48d713d6896e71b7dc6f
ramansh118 <ramanshsingh2005@gmail.com>
2026-09-25 08:59:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (87.15%) - LeetSync

---
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int isPrefixOfWord(string sentence, string searchWord) {
+        int n= sentence.size();
+        int n1=searchWord.size();
+        int i=0;
+        int j=0;
+        int c=1;
+        while (i<n and j<n1){
+            if (sentence[i]== ' ' ){
+                i++;
+                c++;
+                j=0;
+            }else if(sentence[i]==searchWord[j]){
+                i++;
+                j++;
+                if (j==n1){
+                    int y=i;
+                    if (i - n1 == 0 || sentence[i - n1 - 1] == ' ') {
+                        return c;
+                    }
+                    else{
+                        j=0;
+                        continue;
+                    }
+                }
+            }else{
+                j=0;
+                i++;
+            }
+        }
+        return -1;
+    }
+       
+    
+};
